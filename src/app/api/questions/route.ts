f1e770c1407dc798e0f6baeb9bcdc802bb77c75d
Balamathias <106307447+Balamathias@users.noreply.github.com>
2023-12-07 09:27:47 +0100

    Update route.ts

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -4,8 +4,8 @@ import { QuizSchema } from "@/lib/validators/QuizSchema";
 import { NextResponse } from "next/server";
 import { ZodError } from "zod";
 
-export const runtime = "nodejs";
-// export const maxDuration = 10;
+export const runtime = "edge";
+export const maxDuration = 500;
 
 export async function POST(req: Request, res: Response) {
   try {
